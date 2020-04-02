9d1e662182ba391bafb3c6f78e28216168a65c5e
Michael W <baddaemon87@gmail.com>
2020-04-02 13:13:32 +0200

    Updater: Delete updates only after refreshing the notification
    * Looks like we first need to process the notification before we can delete
      an update
    
    Change-Id: I49f6c03a109f908e8a7b01f02b821a68ab273af3

--- src/org/candy/updater/controller/UpdaterService.java
+++ src/org/candy/updater/controller/UpdaterService.java
@@ -23,6 +23,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
@@ -30,6 +31,7 @@ import android.text.format.Formatter;
 import android.util.Log;
 import androidx.core.app.NotificationCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import androidx.preference.PreferenceManager;
 
 import org.candy.updater.R;
 import org.candy.updater.UpdaterReceiver;
@@ -403,6 +405,13 @@ public class UpdaterService extends Service {
                 mNotificationBuilder.setOngoing(false);
                 mNotificationBuilder.setAutoCancel(true);
                 mNotificationManager.notify(NOTIFICATION_ID, mNotificationBuilder.build());
+
+                SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+                boolean deleteUpdate = pref.getBoolean(Constants.PREF_AUTO_DELETE_UPDATES, false);
+                if (deleteUpdate) {
+                    mUpdaterController.deleteUpdate(update.getDownloadId());
+                }
+
                 tryStopSelf();
                 break;
             }
